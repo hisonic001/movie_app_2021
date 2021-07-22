a047a8ce767a4a467d1b6ba4f7cc95be849caf88
Jun <hisonic001@naver.com>
2021-07-22 16:23:12 +0900

    movie_app_detail+

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ".Navigation.css";
+import "./Navigation.css";
 
 function Navigation() {
   return (
-    <div className="Nav">
+    <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
